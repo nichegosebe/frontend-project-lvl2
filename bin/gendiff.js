0daf99726aeaf6dba39dd9b00d6bc07f0f62adb2
Ilya <t788831@gmail.com>
2020-10-10 22:45:14 +0300

    add | lint config + file reading functions + node v14.x

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,8 +1,14 @@
 #!/usr/bin/env node
 
 import program from 'commander';
+import genDiff from './src/index.js';
 
 program
   .version('0.0.1')
+  .arguments('<filepath1> <filepath2>')
   .description('Compares two configuration files and shows a difference.')
+  .action((filePath1, filePath2) => {
+    console.log(genDiff(filePath1, filePath2) || 'Something went wrong!');
+  })
+  .option('-f, --format', 'output format')
   .parse(process.argv);
