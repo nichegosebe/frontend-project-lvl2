0549b1e731cb50db271542ad535cf0810b18c781
Ilya <t788831@gmail.com>
2020-10-17 21:10:04 +0300

    step 5,6 | add ini, yaml parsers

--- src/parseFile.js
+++ src/parseFile.js
@@ -1,9 +1,37 @@
 import { readFileSync } from 'fs';
-import { resolve } from 'path';
+import { resolve, extname } from 'path';
+import yaml from 'js-yaml';
+import ini from 'ini';
 
 const readDataFromFile = (filePath) => {
   const fullFilePath = resolve(process.cwd(), filePath);
   return readFileSync(fullFilePath, 'UTF-8');
 };
 
-export default (filePath) => JSON.parse(readDataFromFile(filePath));
+const jsonParser = (data) => JSON.parse(data);
+
+const yamlParser = (data) => {
+  const res = yaml.safeLoad(data);
+  return res && res.length > 0 ? res[0] : {};
+};
+
+const iniParser = (data) => {
+  const res = ini.safe(data);
+  return res;
+};
+
+export default (filePath) => {
+  const fileExtension = extname(filePath);
+  switch (fileExtension) {
+    case '.json':
+      return jsonParser(readDataFromFile(filePath));
+    case '.yaml':
+      return yamlParser(readDataFromFile(filePath));
+    case '.yml':
+      return yamlParser(readDataFromFile(filePath));
+    case '.ini':
+      return iniParser(readDataFromFile(filePath));
+    default:
+      throw new Error(`Unknown file format ${fileExtension}`);
+  }
+};
