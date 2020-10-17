e5786d9fbfdc919f369002d38a23776cbc3927c8
Ilya <t788831@gmail.com>
2020-10-17 18:49:12 +0300

    fix | file parsing module reduce

--- src/parseFile.js
+++ src/parseFile.js
@@ -1,22 +1,9 @@
-import { existsSync, readFileSync } from 'fs';
+import { readFileSync } from 'fs';
 import { resolve } from 'path';
 
 const readDataFromFile = (filePath) => {
   const fullFilePath = resolve(process.cwd(), filePath);
-  try {
-    if (existsSync(fullFilePath)) {
-      try {
-        return readFileSync(fullFilePath, 'UTF-8');
-      } catch (err) {
-        console.error(`Error reading JSON data from file ${filePath}`, err);
-      }
-    } else {
-      console.error(`Error: file ${filePath} is not found!`);
-    }
-  } catch (err) {
-    console.error(`Error checking if file ${filePath} exists!`);
-  }
-  return false;
+  return readFileSync(fullFilePath, 'UTF-8');
 };
 
 export default (filePath) => JSON.parse(readDataFromFile(filePath));
