6296cef857b7505dec62384c4debb8a93b2d7b05
Ilya <t788831@gmail.com>
2020-10-18 16:59:44 +0300

    parsefile edit

--- src/parsefile.js
+++ src/parsefile.js
@@ -8,18 +8,19 @@ const readDataFromFile = (filePath) => {
   return readFileSync(fullFilePath, 'UTF-8');
 };
 
+// const mapParser
+
 const jsonParser = (data) => JSON.parse(data);
 
 const yamlParser = (data) => yaml.safeLoad(data);
 
-const iniParser = (data) => {
-  const res = ini.parse(data);
-  return res;
-};
+const iniParser = (data) => ini.parse(data);
 
 export default (filePath) => {
   const fileExtension = extname(filePath);
   switch (fileExtension) {
+    case '':
+      return jsonParser(readDataFromFile(filePath));
     case '.json':
       return jsonParser(readDataFromFile(filePath));
     case '.yaml':
