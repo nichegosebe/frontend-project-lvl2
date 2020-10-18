cd79102c0dd6208bb7ef9c477dfc9e2bfade139b
Ilya <t788831@gmail.com>
2020-10-18 22:15:46 +0300

    remove json parser module

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,9 +1,8 @@
 import formatPlain from './plain.js';
 import formatStylish from './stylish.js';
-import formatJSON from './json.js';
 
 export default (formatName) => {
   if (formatName === 'plain') return formatPlain;
-  if (formatName === 'json') return formatJSON;
+  if (formatName === 'json') return JSON.stringify;
   return formatStylish;
 };
--- src/formatters/json.js
+++
@@ -1,5 +0,0 @@
-// import * as STATE from '../constants.js';
-
-const formatJSON = (diffArray) => JSON.stringify(diffArray);
-
-export default formatJSON;
