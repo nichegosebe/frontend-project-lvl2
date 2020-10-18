f62ec44e2f7eb8297eded3704cadd9341a6adfcf
Ilya <t788831@gmail.com>
2020-10-18 13:24:22 +0300

    tests and fixtures update

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -7,56 +7,33 @@ const moduleFileName = fileURLToPath(import.meta.url);
 const absolutePath = dirname(moduleFileName);
 const getFixturePath = (fileName) => join(absolutePath, '..', '__tests__/__fixtures__', fileName);
 
-describe('Test files parsing:', () => {
-  test('- both arguments missing', () => {
-    expect(() => {
-      genDiff();
-    }).toThrow();
-  });
+const formats = [['.json'], ['.yaml'], ['.ini']];
+const formatters = [['stylish'], ['plain']];
 
-  test('- non-existings first file', () => {
-    expect(() => {
-      genDiff(getFixturePath('noExistingFixture.json'), getFixturePath('flat2.json'));
-    }).toThrowError('ENOENT');
-  });
-
-  test('- non-existings second file', () => {
-    expect(() => {
-      genDiff(getFixturePath('flat1.json'), getFixturePath('noExistingFixture.json'));
-    }).toThrowError('ENOENT');
+describe('Tests with empty fixtures', () => {
+  test.each(formats)('.test with %s format', (format) => {
+    expect(
+      genDiff(getFixturePath(`empty${format}`), getFixturePath(`empty${format}`), 'stylish'),
+    ).toEqual('{}');
   });
 });
 
-describe('Test genDiff:', () => {
-  test('- two empty JSON files', () => {
-    expect(genDiff(getFixturePath('empty.json'), getFixturePath('empty.json'))).toEqual('{\n\n}');
-  });
-
-  test('- two non-empty JSON files', () => {
-    const diff = genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'));
-    expect(diff).not.toEqual('{}');
-    expect(diff).not.toBeNull();
-    expect(diff).not.toBeUndefined();
-    expect(diff).toMatchSnapshot();
-  });
-
-  test('- two empty YAML files', () => {
-    expect(genDiff(getFixturePath('empty.yaml'), getFixturePath('empty.yaml'))).toEqual('{\n\n}');
-  });
-
-  test('- two non-empty YAML files', () => {
-    const diff = genDiff(getFixturePath('flat1.yaml'), getFixturePath('flat2.yaml'));
-    expect(diff).not.toEqual('{}');
-    expect(diff).not.toBeNull();
-    expect(diff).not.toBeUndefined();
-    expect(diff).toMatchSnapshot();
-  });
-
-  test('- two non-empty YAML files with different extensions', () => {
-    const diff = genDiff(getFixturePath('flat1.yaml'), getFixturePath('flat2.yml'));
+describe('Tests with both non-empty fixtures', () => {
+  test.each(formats)('.test with %s format', (format) => {
+    const diff = genDiff(getFixturePath(`file1${format}`), getFixturePath(`file2${format}`));
     expect(diff).not.toEqual('{}');
     expect(diff).not.toBeNull();
     expect(diff).not.toBeUndefined();
     expect(diff).toMatchSnapshot();
   });
 });
+
+// describe('Snapshot tests with both non-empty fixtures', () => {
+//   test.each(bothNonEmptyFixtures)('.test with %s, %s', (file1, file2) => {
+//     const diff = genDiff(getFixturePath(file1), getFixturePath(file2));
+//     expect(diff).not.toEqual('{}');
+//     expect(diff).not.toBeNull();
+//     expect(diff).not.toBeUndefined();
+//     expect(diff).toMatchSnapshot();
+//   });
+// });
