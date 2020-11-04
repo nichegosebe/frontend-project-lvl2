33af2f007cedbf643a63c0f1611112480cc97a33
Ilya <t788831@gmail.com>
2020-11-04 17:10:22 +0300

    read data optimization

--- src/index.js
+++ src/index.js
@@ -4,18 +4,15 @@ import buildTree from './treebuilder.js';
 import parse from './parsers.js';
 import format from './formatters/index.js';
 
-const getAbsolutePath = (filePath) => resolve(process.cwd(), filePath);
-
-const getData = (filePath, dataType) => parse(readFileSync(getAbsolutePath(filePath), 'UTF-8'), dataType);
-
-const getType = (filePath) => extname(filePath).slice(1);
+const getData = (filePath) => {
+  const dataType = extname(filePath).slice(1);
+  const absolutePath = resolve(process.cwd(), filePath);
+  return parse(readFileSync(absolutePath, 'UTF-8'), dataType);
+};
 
 export default (filePath1, filePath2, formatName = 'stylish') => {
-  const dataType1 = getType(filePath1);
-  const dataType2 = getType(filePath2);
-
-  const data1 = getData(filePath1, dataType1);
-  const data2 = getData(filePath2, dataType2);
+  const data1 = getData(filePath1);
+  const data2 = getData(filePath2);
 
   const tree = buildTree(data1, data2);
 
